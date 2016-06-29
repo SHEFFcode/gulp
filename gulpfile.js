237db14413a848100b1f7ef74015bbb11bba0a99
Yuriy Shefer <sheff@sheffmachine.com>
2016-06-29 07:50:36 -0700

    add css minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,19 @@
-var gulp 		= require('gulp');
-var uglify 		= require('gulp-uglify');
-var livereload 	= require('gulp-livereload');
-var concat 		= require('gulp-concat');
+var gulp 					= require('gulp');
+var uglify 				= require('gulp-uglify');
+var livereload 		= require('gulp-livereload');
+var concat 				= require('gulp-concat');
+var minifyCSS			= require('gulp-minify-css');
 //file paths
-var DIST_PATH		= 'public/dist';
+var DIST_PATH			= 'public/dist';
 var SCRIPTS_PATH 	= 'public/scripts/**/*.js';
-var CSS_PATH 		= 'public/css/**/*.css'
+var CSS_PATH 			= 'public/css/**/*.css'
 
 //styles
 gulp.task('styles', function () {
 	console.log('starting styles task');
 	return gulp.src(CSS_PATH)
 		.pipe(concat('styles.css'))
+		.pipe(minifyCSS())
 		.pipe(gulp.dest(DIST_PATH))
 		.pipe(livereload());
 });
@@ -38,4 +40,5 @@ gulp.task('watch', function () {
 	require('./server.js');
 	livereload.listen();
 	gulp.watch(SCRIPTS_PATH, ['scripts']);
+	gulp.watch(CSS_PATH, ['styles']);
 });
